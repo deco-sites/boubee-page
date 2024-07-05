01d822264c6f7a36e712630e5378c4ddf3fdfdd7
gabrielMatosBoubee <gabriel.boubee@carcara.tech>
2024-07-05 14:54:15 -0300

    fix

--- sections/Lp/Footer.tsx
+++ sections/Lp/Footer.tsx
@@ -7,9 +7,9 @@ export interface Props {
 function Footer({ text }: Props) {
     return (
         <div class="bg-[#010214]">
-            <footer class="container pt-10 border-t border-[#343543]">
+            <footer class="container flex justify-between items-center py-10 border-t border-[#343543]">
                 <p class="text-white">{text}</p>
-                <a class="w-[60px] h-[60px] bg-transparent rounded-full border border-white" href="#top">
+                <a class="w-[60px] h-[60px] bg-transparent flex items-center justify-center flex-shrink-0 rounded-full border border-white" href="#top">
                     <Icon id="ArrowUP" width={36} height={36} />
                 </a>
             </footer>
