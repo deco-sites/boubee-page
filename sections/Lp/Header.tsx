2ab2422dcf3b0853a258505900590234b451c40f
gabrielMatosBoubee <gabriel.boubee@carcara.tech>
2024-07-28 20:28:33 -0300

    chore: update all files

--- sections/Lp/Header.tsx
+++ sections/Lp/Header.tsx
@@ -8,13 +8,15 @@ export interface Props {
 
 function Header({ logo, buttonLink }: Props) {
     return (
-        <header class="fixed h-20 w-full top-0" id="#top">
-            <div class="container justify-between items-center h-full">
-                <Image src={logo} width={107} height={28} />
-                <a href="#waitlist" class="w-[195px] h-[52px] flex items-center justify-center rounded-full bg-transparent text-white">Join the waitlist today!</a>
-                <a class="w-[132px] rounded-full bg-[#3c2790] flex items-center justify-center text-white" href={buttonLink}>👋  Talk to CEO</a>
-            </div>
-        </header>
+        <div style={{ height: 80 }}>
+            <header class="fixed h-20 w-full top-0" id="#top">
+                <div class="container justify-between items-center h-full">
+                    <Image src={logo} width={107} height={28} />
+                    <a href="#waitlist" class="w-[195px] h-[52px] flex items-center justify-center rounded-full bg-transparent text-white">Join the waitlist today!</a>
+                    <a class="w-[132px] rounded-full bg-[#3c2790] flex items-center justify-center text-white" href={buttonLink}>👋  Talk to CEO</a>
+                </div>
+            </header>
+        </div>
     );
 }
 
